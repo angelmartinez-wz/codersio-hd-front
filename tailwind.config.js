eceedcdc39e81b5fdbac2e257f7ae82e023a045b
MitTorales <mitzi.torales@wizeline.com>
2025-02-12 17:46:23 -0600

    (HD): Add classnames library - add custom colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,19 @@
+const colors = require("tailwindcss/colors");
+
 /** @type {import('tailwindcss').Config} */
 // eslint-disable-next-line import/no-anonymous-default-export
 export default {
   content: ["./src/**/*.{html,js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        ...colors,
+        info: "#E1CA31",
+        warning: "#FC9B63",
+        error: "#E45959",
+        success: "#3CAA18",
+      },
+    },
   },
   plugins: [],
 };
