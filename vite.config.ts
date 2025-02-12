01ac94c4bbd1098967122b3fd6953f7c565bdbad
Angel Martinez <angel.martinez@wizeline.com>
2025-02-12 12:26:41 -0600

    Fix tailwindcss loader

--- vite.config.ts
+++
@@ -1,6 +0,0 @@
-import { defineConfig } from "vite";
-import tsconfigPaths from "vite-tsconfig-paths";
-import tailwindcss from "@tailwindcss/vite";
-export default defineConfig({
-  plugins: [tailwindcss(), tsconfigPaths()],
-});
