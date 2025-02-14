0d016d3f414895a0449a06b80bf31f621e599db5
MitTorales <mitzi.torales@wizeline.com>
2025-02-14 17:36:32 -0600

    (HD): remove data

--- src/components/dealershipCard/dealershipCard.jsx
+++ src/components/dealershipCard/dealershipCard.jsx
@@ -1,46 +1,5 @@
 import { useState } from "react";
-import imgDealership from "../../img/img-dealership.png";
 import { useGetDealerships } from "../../hooks/useGetDealerships";
-
-const options = [
-  {
-    id:"1",
-    content: 
-    <>
-      <div className="flex items-center justify-center bg-primary_3/50 text-min font-medium select-none relative h-8 w-8 leading-4 text-[13px] rounded-[64px]">W</div>
-      <div>
-        <h2 className="font-medium mb-1">West Bend Harley-Davidson (2.9mi)</h2>
-        <p className="font-normal">2910 W Washington St</p>
-        <p className="font-normal">West Bend, WI, 53095</p>
-      </div>
-      <img src={imgDealership} alt="dealership" className="object-cover h-20 w-40" />
-    </> 
-  },
-  {
-    id:"2",
-    content: <>
-    <div className="flex items-center justify-center bg-primary_3/50 text-min font-medium select-none relative h-8 w-8 leading-4 text-[13px] rounded-[64px]">W</div>
-    <div>
-      <h2 className="font-medium mb-1">West Bend Harley-Davidson (2.9mi)</h2>
-      <p className="font-normal">2910 W Washington St</p>
-      <p className="font-normal">West Bend, WI, 53095</p>
-    </div>
-    <img src={imgDealership} alt="dealership" className="object-cover h-20 w-40" />
-  </> 
-  },
-  {
-    id:"3",
-    content: <>
-    <div className="flex items-center justify-center bg-primary_3/50 text-min font-medium select-none relative h-8 w-8 leading-4 text-[13px] rounded-[64px]">W</div>
-    <div>
-      <h2 className="font-medium mb-1">West Bend Harley-Davidson (2.9mi)</h2>
-      <p className="font-normal">2910 W Washington St</p>
-      <p className="font-normal">West Bend, WI, 53095</p>
-    </div>
-    <img src={imgDealership} alt="dealership" className="object-cover h-20 w-40" />
-  </> 
-  }
-];
 
 const DealershipCard = () =>{
   const { dealerships, loading, error } = useGetDealerships();
