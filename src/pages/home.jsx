23b232b2eb7ad67c7dbe89b66ddf54ad7ba8b012
MitTorales <mitzi.torales@wizeline.com>
2025-02-18 10:28:19 -0600

    (HD-02) Add notification to home

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,8 +1,7 @@
 import { useState } from "react";
-import Alert from "../components/alert/alert";
-import AlertType from "../components/alert/alert.types";
 import AppointmentCard from "../components/appointmentCard/appointmentCard";
 import CalendarIcon from "../img/calendar";
+import Notification from "../components/notification/notification";
 
 
 
@@ -15,22 +14,7 @@ const Home = () => {
   }
   return (
     <div className="bg-[url(../public/hd-map.png)] bg-center bg-no-repeat bg-cover h-screen">
-    {/* <Alert
-    type={AlertType.ERROR}
-    content="B1900: Air Bag Circuit Short to Battery"
-    />
-    <Alert
-        type={AlertType.INFO}
-        content="B1000:Instrument Panel Warning Lamp Circuit"
-    />
-    <Alert
-        type={AlertType.WARNING}
-        content="P0115:Engine Coolant Temperature"
-    /> */}
-    <Alert
-      type={AlertType.SUCCESS}
-      content="Your next maintenance will be in 15 days!"
-    />
+    <Notification />
 
     {showAppointment && (
       <AppointmentCard handleClose={handleClose}/>
